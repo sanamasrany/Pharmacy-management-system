a3596b969e6439c3733faae1d6f7f081a6704e96
sanamasrany <157195736+sanamasrany@users.noreply.github.com>
2024-01-20 09:20:33 +0300

    Add files via upload

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,25 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\src\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\ASUS\\StudioProjects\\pharmacy_managmentsystem" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\src\\flutter"
+  "PROJECT_DIR=C:\\Users\\ASUS\\StudioProjects\\pharmacy_managmentsystem"
+  "FLUTTER_ROOT=C:\\src\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\ASUS\\StudioProjects\\pharmacy_managmentsystem\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\ASUS\\StudioProjects\\pharmacy_managmentsystem"
+  "FLUTTER_TARGET=C:\\Users\\ASUS\\StudioProjects\\pharmacy_managmentsystem\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "EXTRA_FRONT_END_OPTIONS=--no-sound-null-safety"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\ASUS\\StudioProjects\\pharmacy_managmentsystem\\.dart_tool\\package_config.json"
+)
